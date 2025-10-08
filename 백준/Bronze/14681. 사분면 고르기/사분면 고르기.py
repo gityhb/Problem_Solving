0b7e2a3cc47a9fa1e6bc59291367d801972e234f
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2025-10-08 18:04:52 +0900

    [Bronze V] Title: 사분면 고르기, Time: 32 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,16 @@
+import sys
+
+A=int(sys.stdin.readline())
+B=int(sys.stdin.readline())
+
+if A>0 and B>0:
+    print(1)
+
+elif A<0 and B>0:
+    print(2)
+
+elif A<0 and B<0:
+    print(3)
+
+elif A>0 and B<0:
+    print(4)
