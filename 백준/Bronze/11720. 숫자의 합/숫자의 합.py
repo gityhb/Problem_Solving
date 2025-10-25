bba41a9b9237691cd29473457910e522f4986e37
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2025-10-25 10:10:34 +0900

    [Bronze IV] Title: 숫자의 합, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11720. 숫자의 합/숫자의 합.py
+++ 백준/Bronze/11720. 숫자의 합/숫자의 합.py
@@ -1,20 +1,11 @@
 import sys
 
-#1. 총 갯수
-try:
-    N=int(sys.stdin.readline())
-except:
-    N=0
+N = int(sys.stdin.readline())
 
-#2. 한줄의 문자열로 입력
-try:
-    number=sys.stdin.readline().strip()
-except:
-    number=""
-
+strings = str(sys.stdin.readline()).strip()
 sum = 0
 
-for num in number:
-    sum += int(num)
+for char in strings:
+    sum += int(char)
 
 print(sum)
