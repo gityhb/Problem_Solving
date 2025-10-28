3da2018a91436875fa1826a86a3a8cba2a6871b8
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2025-10-28 14:56:06 +0900

    [Silver II] Title: 최대 힙, Time: 112 ms, Memory: 38340 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.py
@@ -0,0 +1,18 @@
+import sys
+import heapq
+
+N = int(sys.stdin.readline())
+
+heap = []
+
+for i in range(N):
+    x = int(sys.stdin.readline())
+
+    if x != 0:
+        heapq.heappush(heap, -x)
+
+    elif x==0:
+        if heap:
+            print(-heapq.heappop(heap))
+        else:
+            print(0)
