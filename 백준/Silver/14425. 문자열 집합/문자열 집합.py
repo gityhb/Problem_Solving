3cc02958a2d992bcb1a95dcdcd62cade8d357ab9
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2025-12-04 14:46:45 +0900

    [Silver IV] Title: 문자열 집합, Time: 92 ms, Memory: 43240 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -2,33 +2,21 @@ import sys
 
 N, M = map(int, sys.stdin.readline().split())
 
-trie = {}
+list = set()
+find_list = []
 
-#집합 S의 문자열 트라이에 넣기
-for _ in range(N):
-    word = sys.stdin.readline().strip()
-    current = trie
-    for char in word:
-        if char not in current:
-            current[char] = {}
-        current = current[char]
-    
-    current["_end"] = True
+for i in range(N):
+    string = sys.stdin.readline()
+    list.add(string)
 
-count=0
-for _ in range(M):
-    word = sys.stdin.readline().strip()
-    current = trie
-    is_exist = True
+for i in range(M):
+    find = sys.stdin.readline()
+    find_list.append(find)
 
-    #한글자씩 훑음
-    for char in word:
-        if char not in current:
-            is_exist = False
-            break
-        current = current[char]
-    
-    if is_exist and "_end" in current:
-        count+=1
+count = 0
+
+for check in find_list:
+    if check in list:
+        count += 1
 
 print(count)
