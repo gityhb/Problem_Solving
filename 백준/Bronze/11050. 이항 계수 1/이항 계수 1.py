da7a85f9cd3871573b913875abe871752266b514
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2025-12-06 11:15:06 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,5 +1,16 @@
-import sys, math
+import sys
 
 N, K = map(int, sys.stdin.readline().split())
 
-print(math.comb(N, K))
+data = [[0 for _ in range(N+1)] for _ in range(N+1)]
+
+for i in range(N+1):
+    data[i][1] = i
+    data[i][i] = 1
+    data[i][0] = 1
+
+for i in range(2, N+1):
+    for j in range(1, i):
+        data[i][j] = data[i-1][j-1]+data[i-1][j]
+
+print(data[N][K])
