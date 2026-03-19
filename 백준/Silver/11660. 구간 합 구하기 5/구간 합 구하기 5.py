0453d5eea730ecc51a8fd462349707911eb26a72
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2026-03-19 18:27:10 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1196 ms, Memory: 111108 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,31 +1,20 @@
 import sys
 
-# 1. 표의 크기 N과 횟수 M 입력 받기
-N, M= map(int, sys.stdin.readline().split())
+N, M = map(int, sys.stdin.readline().split())
 
-# 2. 누적합 배열 초기화
-D = [[0]*(N+1) for _ in range(N+1)]
+A = [[0] * (N+1) for _ in range(N+1)]
+D = [[0] * (N+1) for _ in range(N+1)]
+results = [0] * M
 
-# 3. 원본 배열 A 입력 및 누적 합 배열 D 채우기
-result = []
 for i in range(1, N+1):
-    row_data = list(map(int, sys.stdin.readline().split()))
+    numbers = list(map(int, sys.stdin.readline().split()))
+    for j in range(1, N+1): 
+        A[i][j] = numbers[j-1]
+        D[i][j] = D[i-1][j] + D[i][j-1] - D[i-1][j-1] + A[i][j]
 
-    for j in range(1, N+1):
-        #원본 값
-        A_ij = row_data[j-1]
-
-        #누적 합 배열
-        D[i][j] = D[i][j-1] + D[i-1][j] - D[i-1][j-1] + A_ij
-    
-# 4. M개의 구간 합
-outputs = []
-for _ in range(M):
+for m in range(M):
     x1, y1, x2, y2 = map(int, sys.stdin.readline().split())
+    results[m] = D[x2][y2] - D[x2][y1-1] - D[x1-1][y2] + D[x1-1][y1-1]
 
-    result = D[x2][y2] - D[x1-1][y2] - D[x2][y1-1] + D[x1-1][y1-1]
-
-    outputs.append(str(result))
-
-# 5. 결과 출력
-sys.stdout.write('\n'.join(outputs)+'\n')
+for i in range(M):
+    print(results[i])
