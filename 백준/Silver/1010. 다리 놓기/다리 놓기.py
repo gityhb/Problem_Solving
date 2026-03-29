200358f92659d8b0a29577c3ac197ebe9edb46a8
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2026-03-29 21:37:26 +0900

    [Silver V] Title: 다리 놓기, Time: 32 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,11 +1,11 @@
+from itertools import combinations
 import sys, math
 
 T = int(sys.stdin.readline())
-
 result = []
 
 for _ in range(T):
-    N, M = map(int, sys.stdin.readline().split())
+    N, M= map(int, sys.stdin.readline().split())
     result.append(math.comb(M, N))
 
 for i in result:
