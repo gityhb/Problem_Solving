0556e008f47ae3c46ac4defe26baab42ed0b7656
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2026-03-29 22:02:31 +0900

    [Silver V] Title: 숫자 카드, Time: 3936 ms, Memory: 111124 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,19 +1,30 @@
 import sys
 
-N = int(sys.stdin.readline()) #숫자 개수
+N = int(sys.stdin.readline())
 
-card_list = set(map(int, sys.stdin.readline().split()))
+num1 = list(map(int, sys.stdin.readline().split()))
 
 M = int(sys.stdin.readline())
 
-card_check = list(map(int, sys.stdin.readline().split()))
+find = list(map(int, sys.stdin.readline().split()))
 
 result = []
+num1.sort()
 
-for check in card_check:
-    if check in card_list:
-        result.append(1)
-    else:
-        result.append(0)
+for i in range(M):
+    start = 0
+    end = N-1
+
+    while start<=end:
+        mid = (start+end)//2
+        if find[i] == num1[mid]:
+            result.append(1)
+            break
+        elif find[i] > num1[mid]:
+            start = mid+1
+        elif find[i] < num1[mid]:
+            end = mid-1
+
+    else: result.append(0)
 
 print(*result)
