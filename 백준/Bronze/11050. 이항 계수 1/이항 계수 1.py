52d8cd72744217b54d37bbfd5dc24cfd2aaebb26
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2026-03-29 21:26:34 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -1,16 +1,15 @@
+from itertools import combinations
 import sys
 
-N, K = map(int, sys.stdin.readline().split())
+N ,K= map(int, sys.stdin.readline().split())
+num = []
 
-data = [[0 for _ in range(N+1)] for _ in range(N+1)]
+for i in range(1, N+1):
+    num.append(i)
 
-for i in range(N+1):
-    data[i][1] = i
-    data[i][i] = 1
-    data[i][0] = 1
+count=0
 
-for i in range(2, N+1):
-    for j in range(1, i):
-        data[i][j] = data[i-1][j-1]+data[i-1][j]
+for i in combinations(num, K):
+    count+=1
 
-print(data[N][K])
+print(count)
