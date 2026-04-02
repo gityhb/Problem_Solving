d88dd2ad18337f348f48c228a6f38bab8ff4861c
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2026-04-02 20:41:53 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 624 ms, Memory: 66048 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -6,29 +6,23 @@ N, M = map(int, sys.stdin.readline().split())
 graph = [[] for _ in range(N+1)]
 
 for _ in range(M):
-    a, b = map(int, sys.stdin.readline().split())
-    
-    graph[a].append(b)
-    graph[b].append(a)
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-for i in range(1, N+1):
-    graph[i].sort()
-
-visited_dfs = [False] * (N+1)
+visited = [False] * (N+1)
+count = 0
 
 def dfs(node):
-    global possible
-    visited_dfs[node] = True
+    visited[node] = True
+    
+    for next in graph[node]:
+        if not visited[next]:
+            dfs(next)
 
-    for neighbor in graph[node]:
-        if not visited_dfs[neighbor]:
-            dfs(neighbor)
-
-component_count = 0
-
-for i in range(1, N + 1):
-    if not visited_dfs[i]:
-        component_count += 1
+for i in range(1, N+1):
+    if not visited[i]:
         dfs(i)
+        count+=1
 
-print(component_count)
+print(count)
