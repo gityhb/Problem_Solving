2090edab8c4c21a8d077e5fcbe55de8064644b01
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2026-04-09 11:00:11 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 572 ms, Memory: 61028 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+
+N, M, R = map(int, sys.stdin.readline().split())
+
+graph = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+#오른차순으로 방문
+for i in range(1, N+1):
+    graph[i].sort(reverse=True)
+
+visited = [0] * (N+1)
+
+def bfs(start):
+    queue = deque([start])
+    count = 1
+    visited[start] = count
+
+    while queue:
+        node = queue.popleft()
+
+        for neighbor in graph[node]:
+            if visited[neighbor] == 0:
+                count += 1
+                visited[neighbor] = count
+                queue.append(neighbor)
+        
+bfs(R)
+
+for i in range(1, N+1):
+    print(visited[i])
