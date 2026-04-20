f5817973fb114225b9031679fb14ccaec3520b64
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2026-04-20 20:13:05 +0900

    [Silver V] Title: 거스름돈, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/14916. 거스름돈/거스름돈.py
@@ -0,0 +1,17 @@
+import sys
+
+n = int(sys.stdin.readline()) #거스름돈 액수
+count= 0
+
+while True:
+    if n%5==0:
+        count+=n//5
+        print(count)
+        break
+    
+    n-=2
+    count += 1
+
+    if n<0:
+        print(-1)
+        break
