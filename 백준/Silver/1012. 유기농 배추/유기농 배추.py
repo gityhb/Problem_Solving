db50e8e5a9147b7cee2cf3b87d641dc4ee1ad6e2
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2026-04-21 13:47:52 +0900

    [Silver II] Title: 유기농 배추, Time: 64 ms, Memory: 34992 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,38 +1,45 @@
 import sys
-sys.setrecursionlimit(10**6)
+from collections import deque
 
-
-T = int(sys.stdin.readline()) #테스트 횟수
-result = []
+T = int(sys.stdin.readline())
 
 dx = [0, 0, -1, 1]
-dy = [1, -1, 0, 0]
-            
+dy = [-1, 1, 0, 0]
+
+result = []
+
+def bfs(y, x):
+    queue = deque()
+    visited[y][x] = True
+    queue.append((y, x))
+
+    while queue:
+        ky, kx = queue.popleft()
+        for i in range(4):
+            nx = kx + dx[i]
+            ny = ky + dy[i]
+            if 0<= nx < M and 0<=ny<N:
+                if not visited[ny][nx] and graph[ny][nx] == 1:
+                    visited[ny][nx] = True
+                    queue.append((ny, nx))
+
 for _ in range(T):
-    count = 0
-    M, N, K = map(int, sys.stdin.readline().split()) #가로, 세로 길이
-    graph=[[0]*M for _ in range(N)]
+
+    count =  0
+    M, N, K = map(int, sys.stdin.readline().split())
+
+    visited = [[False]*M for _ in range(N)]
+    graph = [[0] * M for _ in range(N)]
 
     for _ in range(K):
-        X, Y = map(int, sys.stdin.readline().split())
-        graph[Y][X] =1
-
-    def dfs(x, y):
-        graph[y][x] = 0
-
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-
-            if 0<=nx<M and 0<=ny<N:
-                if graph[ny][nx] == 1:
-                    dfs(nx, ny)
-
-    for y in range(N):
-        for x in range(M):
-            if graph[y][x] == 1:
-                dfs(x, y)
-                count+=1 
+        a, b = map(int, sys.stdin.readline().split())
+        graph[b][a] = 1
+    
+    for i in range(N):
+        for j in range(M):
+            if not visited[i][j] and graph[i][j]==1:
+                bfs(i, j)
+                count+=1
     
     result.append(count)
 
