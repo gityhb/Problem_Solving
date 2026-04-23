5f3f737c43270f8414b8e1bebfc1d69965d17dd9
YOON HYEBIN <121148492+gityhb@users.noreply.github.com>
2026-04-23 14:58:50 +0900

    [Silver IV] Title: ATM, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,14 +1,15 @@
 import sys
 
 N = int(sys.stdin.readline())
-
 times = list(map(int, sys.stdin.readline().split()))
 
 times.sort()
 
-hap = [times[0]] *(N)
+hap = [times[0]] * N
+result = [times[0]] * N
 
 for i in range(1, N):
-    hap[i]=hap[i-1]+times[i]
+    hap[i] = hap[i-1] + times[i]
+    result[i] = result[i-1] + hap[i]
 
-print(sum(hap))
+print(result[N-1])
